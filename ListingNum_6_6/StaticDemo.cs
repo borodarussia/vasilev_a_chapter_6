4b950dc3b75b89061fe212042567b4faaf1092a2
Ivan Kudryashov <ivan.kudryash1337@gmail.com>
2023-12-22 09:07:48 +0400

    add listing num 6.6

---
+++ ListingNum_6_6/StaticDemo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListingNum_6_6
+{
+    /*
+     * Листинг 6.6
+     * Знакомство со статическими полями и методами
+     */
+
+    // класс со статическим полем и методом
+    class MyClass
+    {
+        // статическое поле:
+        public static int code = 100;
+        // статический мтеод:
+        public static void show()
+        {
+            Console.WriteLine("Статическое поле: {0}", code);
+        }
+    }
+    // класс с главным методом:
+    internal class StaticDemo
+    {
+        // главный метод:
+        static void Main(string[] args)
+        {
+            // вызов статического метода:
+            MyClass.show();
+            // обращение к статическому полю:
+            MyClass.code = 200;
+            // вызов статического метода:
+            MyClass.show();
+
+            // Завершение программы
+            Console.Write("\n\n\nДля завершения работы программы нажмите Enter...");
+            Console.ReadLine();
+        }
+    }
+}
