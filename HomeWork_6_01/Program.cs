ba6113e7086c7203bed7cc0a0957df617e47beaf
1 <1>
2023-12-25 17:05:00 +0400

    complete hw num1 and num2

---
+++ HomeWork_6_01/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace HomeWork_6_01
+{
+    /*
+     * Программа с классом, в котором есть закрытое символьное поле
+     * и три открытых метода.
+     * Один из методов позволяет присвоить значение полю.
+     * Еще один метод при вызове возвращает результатом код символа.
+     * Третий метод позволяет вывести в консольное окно
+     * символ (значение поля) и его код.
+     */
+    class MyClass
+    {
+
+        private char _symb;
+
+        public void set_symb(char symbol)
+        {
+            _symb = symbol;
+        }
+
+        public char get_symb()
+        {
+            return _symb;
+        }
+
+        public void show_symb()
+        {
+            Console.WriteLine($"Символ: {_symb}\nКод символа: {(int)_symb}");
+        }
+
+        public MyClass()
+        {
+            _symb = 'a';
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass A = new MyClass();
+
+            A.show_symb();
+
+            A.set_symb('b');
+            Console.WriteLine();
+            A.show_symb();
+
+            Console.WriteLine($"\nСимвол объекта A: {A.get_symb()}");
+
+            // Завершение программы
+            Console.Write("\n\n\nДля завершения работы программы нажмите Enter...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HomeWork_6_02/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_6_02
+{
+    /*
+     * Программа с классом, у которого есть два символьных поля
+     * и метод. Он не возвращает результат, и у него нет аргументов.
+     * При вызове метод выводит в консольное окно все символы из
+     * кодовой таблицы, котороые находятся "между" символами, являющимися
+     * значениями полей объекта (из которого вызывается метод). Например,
+     * если полям объекта присвоены значения 'A' и 'D', то при вызове
+     * метода в консольное окно должны выводиться все символы от 'A' до 'D'
+     * включительно.
+     */
+    class MyClass
+    {
+        private char _startSymb;
+        private char _endSymb;
+
+        public MyClass(char startSymb, char endSymb)
+        {
+            _startSymb = startSymb;
+            _endSymb = endSymb;
+        }
+        public MyClass(char startSymb)
+        {
+            _startSymb = startSymb;
+            _endSymb = (char)((int)startSymb + 2);
+        }
+        public void show()
+        {
+            int startSymbCode = (int)_startSymb;
+            int endSymbCode = (int) _endSymb;
+            for(int i = 0; i <= (endSymbCode - startSymbCode); i++)
+            {
+                if(i == 0)
+                {
+                    Console.Write($"Список символов от {_startSymb} до {_endSymb}: [ {(char)(startSymbCode + i)} |");
+                }
+                else if(i == (endSymbCode - startSymbCode))
+                {
+                    Console.WriteLine($" {(char)(startSymbCode + i)} ]\n");
+                }
+                else
+                {
+                    Console.Write($" {(char)(startSymbCode + i)} |");
+                }
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass A = new MyClass('z');
+            A.show();
+
+
+            // Завершение программы
+            Console.Write("\n\n\nДля завершения работы программы нажмите Enter...");
+            Console.ReadLine();
+        }
+    }
+}
