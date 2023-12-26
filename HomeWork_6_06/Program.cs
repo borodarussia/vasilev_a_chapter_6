d8931d98a8d778d6665a278ab3fea08e00fe3ea5
Ivan Kudryashov <boroda1337russia@gmail.com>
2023-12-26 14:01:30 +0400

    complete hw num 7

---
+++ HomeWork_6_06/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_6_06
+{
+    class MyClass
+    {
+        private int _maxValue;
+        private int _minValue;
+
+        public int maxValue
+        {
+            set { _maxValue = value; }
+            get { return _maxValue; }
+        }
+        public int minValue
+        {
+            set { _minValue = value; }
+            get { return _minValue; }
+        }
+        public void setValues(int x)
+        {
+            maxValue = x;
+            minValue = x - 5;
+        }
+        public void setValues(int x, int y)
+        {
+            if (x >= y)
+            {
+                maxValue = x;
+                minValue = y;
+                return;
+            }
+
+            maxValue = y;
+            minValue = x;
+        }
+        public void show()
+        {
+            Console.WriteLine($"Максимальное значение:\t{maxValue}\n" +
+                $"Минимальное значение:\t{minValue}");
+        }
+        public MyClass()
+        {
+            setValues(10);
+        }
+        public MyClass(int x)
+        {
+            setValues(x);
+        }
+        public MyClass(int x, int y)
+        {
+            setValues(x, y);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass A = new MyClass();
+            MyClass B = new MyClass(100);
+            MyClass C = new MyClass(200, 300);
+
+            A.show();
+            B.show();
+            C.show();
+
+            // Завершение программы
+            Console.Write("\n\n\nДля завершения работы программы нажмите Enter...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HomeWork_6_07/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_6_07
+{
+    class MyClass
+    {
+        private string _txt;
+        private char _symbol;
+        public string txt
+        {
+            get { return _txt; }
+            set { _txt = value; }
+        }
+        public char symbol
+        {
+            get { return _symbol; }
+            set { _symbol = value; }
+        }
+
+
+        public void inputInfo(string t)
+        {
+            txt = t;
+        }
+        public void inputInfo(char s)
+        {
+            symbol = s;
+        }
+        public void inputInfo(char[] sArr)
+        {
+            string exportTxt = "";
+            foreach(char s in sArr)
+            {
+                exportTxt += s;
+            }
+            txt = exportTxt;
+        }
+        public void show()
+        {
+            Console.WriteLine($"Строка: {txt}\nСимвол: {symbol}");
+        }
+        public MyClass()
+        {
+            txt = "example";
+            symbol = 'a';
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass A = new MyClass();
+            MyClass B = new MyClass();
+            MyClass C = new MyClass();
+
+            A.inputInfo("Проверочный текст");
+            A.show();
+            A.inputInfo('l');
+            A.show();
+            char[] symbArra = { 'l', 'a', 'z', 'y' };        
+            A.inputInfo(symbArra);
+            A.show();
+
+            // Завершение программы
+            Console.Write("\n\n\nДля завершения работы программы нажмите Enter...");
+            Console.ReadLine();
+        }
+    }
+}
